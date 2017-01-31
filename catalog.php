eabd2c5a929101799d038e6fe1fcdeb5f9ec41a9
Etienne Fichot <fichotetienne@gmail.com>
2017-01-31 18:13:15 +0100

    split function PHP

--- catalog.php
+++ catalog.php
@@ -1,4 +1,6 @@
 <?php
+include("includes/data.php");
+include("includes/function.php");
 if (isset($_GET["cat"]))
 {
     if ($_GET["cat"] == "books"){
@@ -15,7 +17,17 @@ if (isset($_GET["cat"]))
 else
     $pageTitle = "Catalog";
 include("includes/header.php"); ?>
-<div class="section page">
+<div class="section catalog page">
+    <div class="wrapper">
         <h1><?php echo $pageTitle ?></h1>
+        <ul class="items">
+            <?php
+            foreach ($catalog as $id => $item)
+            {
+                echo ft_get_html_by_id($id, $item);
+            }
+            ?>
+        </ul>
+    </div>
 </div>
 <?php include("includes/footer.php"); ?>
--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+include("includes/data.php");
+include("includes/function.php");
 $pageTitle = "Personal Media Library";
 $currentPage = null;
 include("includes/header.php");?>
@@ -8,7 +10,12 @@ include("includes/header.php");?>
 
 				<h2>May we suggest something?</h2>
 					<ul class="items">
-						<li><a href="details.php?id=201"><img src="img/media/forest_gump.jpg" alt="Forrest Gump"><p>View Details</p></a></li><li><a href="details.php?id=204"><img src="img/media/princess_bride.jpg" alt="The Princess Bride"><p>View Details</p></a></li><li><a href="details.php?id=302"><img src="img/media/elvis_presley.jpg" alt="Elvis Forever"><p>View Details</p></a></li><li><a href="details.php?id=303"><img src="img/media/garth_brooks.jpg" alt="No Fences"><p>View Details</p></a></li>
+						<?php
+						foreach ($catalog as $id => $item)
+						{
+							echo ft_get_html_by_id($id, $item);
+						}
+						?>
 					</ul>
 			</div>
 
